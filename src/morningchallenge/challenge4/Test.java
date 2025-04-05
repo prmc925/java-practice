70abb27051a8cf1772a113a5d02e25fdbcb70a0c
= <=>
2025-04-05 13:02:15 +0100

    Morning Challenge3 And real time example of inheritance and abstraction

---
+++ src/morningchallenge/challenge4/Test.java
@@ -0,0 +1,11 @@
+package morningchallenge.challenge4;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Test extends PrintMyName{
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.printName(15, "Jay");
+    }
+}
